448fc97f340887de82847a9b73cc11c7bcc0a73f
Ricardo Gomez_Ulmke <ricardojosegomezulmke@gmail.com>
2022-09-26 12:20:19 +0200

    get the original payload for avro schemas

--- ep-asyncapi/test/specs/test-pass/schema-format.spec.ts
+++ ep-asyncapi/test/specs/test-pass/schema-format.spec.ts
@@ -4,11 +4,18 @@ import path from 'path';
 import { TestLogger } from '../../lib/TestLogger';
 import { TestContext } from '../../lib/TestContext';
 import TestConfig from '../../lib/TestConfig';
+import { TestUtils } from '../../lib/TestUtils';
 
 import EpAsyncApiDocumentService from '../../../src/services/EpAsyncApiDocumentService';
 import { EpAsyncApiDocument, T_EpAsyncApiMessageDocumentMap } from '../../../src/documents/EpAsyncApiDocument';
 import { EpAsyncApiError } from '../../../src/utils/EpAsyncApiErrors';
 import { E_EpAsyncApiSchemaFormatType } from '../../../src/documents/EpAsyncApiMessageDocument';
+import { Validator } from 'jsonschema';
+
+import avro from "avsc";
+
+
+
 
 const scriptName: string = path.basename(__filename);
 TestLogger.logMessage(scriptName, ">>> starting ...");
@@ -62,6 +69,38 @@ describe(`${scriptName}`, () => {
           else if(key.includes('avro')) expectedSchemaFormatType = E_EpAsyncApiSchemaFormatType.APPLICATION_AVRO;
           const message = `\nkey=${key}, \nexpectedSchemaFormatType=${expectedSchemaFormatType}, \nepAsyncApiSchemaFormatType=${epAsyncApiSchemaFormatType}`;
           expect(epAsyncApiSchemaFormatType, message).to.eq(expectedSchemaFormatType);
+        }
+      } catch(e) {
+        expect(false, TestLogger.createTestFailMessageForError('failed', e)).to.be.true;
+      }
+    });
+
+    it(`${scriptName}: should get correct schema payload`, async () => {
+      try {
+        const epAsyncApiMessageDocumentMap: T_EpAsyncApiMessageDocumentMap = AsyncApiDocument.getEpAsyncApiMessageDocumentMap();
+        for(const [key, epAsyncApiMessageDocument] of epAsyncApiMessageDocumentMap) {
+          const epAsyncApiSchemaFormatType: E_EpAsyncApiSchemaFormatType = epAsyncApiMessageDocument.getSchemaFormatType();
+          const payloadAnySchema: any = epAsyncApiMessageDocument.getSchemaAsSanitizedJson();
+          const message = `\nepAsyncApiSchemaFormatType=${epAsyncApiSchemaFormatType},\n payloadAnySchema=${JSON.stringify(payloadAnySchema, null, 2)}`;
+          switch(epAsyncApiSchemaFormatType) {
+            case E_EpAsyncApiSchemaFormatType.APPLICATION_AVRO:
+              // not really necessary, already validated in async api parser
+              const at: avro.Type = avro.Type.forSchema(payloadAnySchema);
+              // at.typeName
+              // expect(false, message).to.be.true;
+              expect(at.typeName, 'typeName mismatch').to.equal('record');
+              break;
+            case E_EpAsyncApiSchemaFormatType.APPLICATION_JSON:
+              // not really necessary, already validated in async api parser
+
+              // validate schema
+              const v: Validator = new Validator();
+              v.addSchema(payloadAnySchema);
+              // expect(false, message).to.be.true;
+              break;
+            default:
+              TestUtils.assertNever(scriptName, epAsyncApiSchemaFormatType);
+          }
         }
       } catch(e) {
         expect(false, TestLogger.createTestFailMessageForError('failed', e)).to.be.true;
